31cc88749db174a7ee13cfe0c1d31d7f36f7f750
Zyao89 <zyao89@gmail.com>
2019-12-30 00:37:27 +0800

    :fix: circleci

--- test/bin.test.js
+++ test/bin.test.js
@@ -3,9 +3,6 @@ process.env.MICRO_APP_TEST = 'true';
 
 /* global expect */
 
-const TIMEOUT = 1000 * 10;
-const CWD = process.cwd();
-
 describe('VuePress Bin test', () => {
 
     it('help run', async () => {
@@ -16,18 +13,6 @@ describe('VuePress Bin test', () => {
         } catch (error) {
             console.warn(error);
         }
-
-
-        // const execa = require('execa');
-        // const { code, stderr } = execa('node', [ 'node_modules/.bin/micro-app', 'vuepress' ], {
-        //     cwd: CWD,
-        //     timeout: TIMEOUT,
-        //     env: process.env,
-        // });
-
-        // if (code && stderr) {
-        //     throw new Error(stderr);
-        // }
     });
 
     it('dev run', async () => {
@@ -38,18 +23,6 @@ describe('VuePress Bin test', () => {
         } catch (error) {
             console.warn(error);
         }
-
-        // const execa = require('execa');
-
-        // const { code, stderr } = execa('node', [ 'node_modules/.bin/micro-app', 'vuepress', 'dev' ], {
-        //     cwd: CWD,
-        //     timeout: TIMEOUT,
-        //     env: process.env,
-        // });
-
-        // if (code && stderr) {
-        //     throw new Error(stderr);
-        // }
     });
 
     it('build run', async () => {
@@ -60,19 +33,6 @@ describe('VuePress Bin test', () => {
         } catch (error) {
             console.warn(error);
         }
-
-
-        // const execa = require('execa');
-
-        // const { code, stderr } = execa('node', [ 'node_modules/.bin/micro-app', 'vuepress', 'build' ], {
-        //     cwd: CWD,
-        //     timeout: TIMEOUT,
-        //     env: process.env,
-        // });
-
-        // if (code && stderr) {
-        //     throw new Error(stderr);
-        // }
     });
 
     it('dev run docs', async () => {
@@ -83,19 +43,5 @@ describe('VuePress Bin test', () => {
         } catch (error) {
             console.warn(error);
         }
-
-
-        // const execa = require('execa');
-
-        // const { code, stderr } = execa('node', [ 'node_modules/.bin/micro-app', 'vuepress', 'dev docs' ], {
-        //     cwd: CWD,
-        //     timeout: TIMEOUT,
-        //     env: process.env,
-        // });
-
-        // if (code && stderr) {
-        //     throw new Error(stderr);
-        // }
     });
-
 });
