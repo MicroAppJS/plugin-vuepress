15caf23910ded15e7ed20fc07dce688deee2aad3
Zyao89 <zyao89@gmail.com>
2019-12-30 00:10:16 +0800

    :fix: circleci

--- test/bin.test.js
+++ test/bin.test.js
@@ -9,9 +9,9 @@ const CWD = process.cwd();
 describe('VuePress Bin test', () => {
 
     it('help run', async () => {
-        const shelljs = require('shelljs');
+        const execa = require('execa');
 
-        const { code, stderr } = shelljs.exec('node node_modules/.bin/micro-app vuepress', {
+        const { code, stderr } = execa('node', [ 'node_modules/.bin/micro-app', 'vuepress' ], {
             cwd: CWD,
             timeout: TIMEOUT,
             env: process.env,
@@ -23,9 +23,9 @@ describe('VuePress Bin test', () => {
     });
 
     it('dev run', async () => {
-        const shelljs = require('shelljs');
+        const execa = require('execa');
 
-        const { code, stderr } = shelljs.exec('node node_modules/.bin/micro-app vuepress dev', {
+        const { code, stderr } = execa('node', [ 'node_modules/.bin/micro-app', 'vuepress', 'dev' ], {
             cwd: CWD,
             timeout: TIMEOUT,
             env: process.env,
@@ -37,9 +37,9 @@ describe('VuePress Bin test', () => {
     });
 
     it('build run', async () => {
-        const shelljs = require('shelljs');
+        const execa = require('execa');
 
-        const { code, stderr } = shelljs.exec('node node_modules/.bin/micro-app vuepress build', {
+        const { code, stderr } = execa('node', [ 'node_modules/.bin/micro-app', 'vuepress', 'build' ], {
             cwd: CWD,
             timeout: TIMEOUT,
             env: process.env,
@@ -51,9 +51,9 @@ describe('VuePress Bin test', () => {
     });
 
     it('dev run docs', async () => {
-        const shelljs = require('shelljs');
+        const execa = require('execa');
 
-        const { code, stderr } = shelljs.exec('node node_modules/.bin/micro-app vuepress dev docs', {
+        const { code, stderr } = execa('node', [ 'node_modules/.bin/micro-app', 'vuepress', 'dev docs' ], {
             cwd: CWD,
             timeout: TIMEOUT,
             env: process.env,
