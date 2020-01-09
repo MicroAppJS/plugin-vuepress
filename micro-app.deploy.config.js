0be2cfe46e1f29c8e8a8d0c2e2c7f468bb060440
Zyao89 <zyao89@gmail.com>
2020-01-10 00:29:36 +0800

    :update: change deploy yml

---
+++ micro-app.deploy.config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    repo: 'git@github.com:MicroAppJS/plugin-vuepress.git',
+    branch: 'gh-pages',
+    dist: 'docs/.vuepress/dist',
+    // message: 'AUTO TEST',
+    // cname: 'www.2o3t.cn',
+};
