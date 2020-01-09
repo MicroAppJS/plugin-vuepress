7a5dc58057278438a7a5c8be285df48374c64b9c
Zyao89 <zyao89@gmail.com>
2020-01-10 02:20:17 +0800

    :update: change deploy config

--- micro-app.deploy.config.js
+++ micro-app.deploy.config.js
@@ -1,9 +1,14 @@
 'use strict';
 
+let repoToken = 'git';
+if (process.env.GITHUB_TOKEN && process.env.GITHUB_TOKEN !== 'undefined') {
+    repoToken = `x-access-token:${process.env.GITHUB_TOKEN}`;
+} else if (process.env.ACCESS_TOKEN && process.env.ACCESS_TOKEN !== 'undefined') {
+    repoToken = process.env.ACCESS_TOKEN;
+}
+
 module.exports = {
-    repo: 'git@github.com:MicroAppJS/plugin-vuepress.git',
+    repo: `https://${repoToken}@github.com/MicroAppJS/plugin-vuepress.git`,
     branch: 'gh-pages',
     dist: 'docs/.vuepress/dist',
-    // message: 'AUTO TEST',
-    // cname: 'www.2o3t.cn',
 };
