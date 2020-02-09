87047e2b69a6323a71a72a73df3af0233cb82c2b
Zyao89 <zyao89@gmail.com>
2020-02-10 03:21:48 +0800

    wrench: update config

--- micro-app.release.config.js
+++ micro-app.release.config.js
@@ -10,6 +10,8 @@ module.exports = {
     git: {
         commitMessage: 'chore: release v%s',
         tagName: 'v%s',
+        requireUpstream: false,
+        push: false,
     },
     npm: {
         publish: true,
