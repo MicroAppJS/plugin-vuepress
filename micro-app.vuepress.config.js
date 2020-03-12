c60d48cf0c70640994cf10071299274e9a3ee5eb
Zyao89 <zyao89@gmail.com>
2020-03-12 15:58:17 +0800

    refactor: art

--- micro-app.vuepress.config.js
+++ micro-app.vuepress.config.js
@@ -8,6 +8,15 @@ module.exports = {
     author: 'Zyao89',
     // siteUrl: '',
     sourceDir: 'docs',
+    head: [
+        [ 'link', { rel: 'icon', href: '/icons/favicon.png' }],
+        [ 'link', { rel: 'manifest', href: '/manifest.json' }],
+        [ 'meta', { name: 'theme-color', content: '#EDB65B' }],
+        [ 'meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        [ 'meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+        [ 'link', { rel: 'apple-touch-icon', href: '/icons/favicon@0,5x.png' }],
+        [ 'link', { rel: 'mask-icon', href: '/icons/favicon.svg', color: '#EDB65B' }],
+    ],
     locales: {
         '/zh/': {
             lang: 'zh-CN',
