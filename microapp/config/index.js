d6b5b80cf3153c213897355fe22bef6a5e401634
Zyao89 <zyao89@gmail.com>
2020-03-16 23:43:46 +0800

    fix: fix root path

--- microapp/config/index.js
+++ microapp/config/index.js
@@ -1,11 +1,14 @@
 'use strict';
 
+const path = require('path');
+const ROOT = path.resolve(__dirname, '../../');
+
 const config = {};
 
 // 兼容两种启动方式
 if (!process.env.MICRO_APP_VUEPRESS_DIRECT_RUNNING) {
     config.plugins = [
-        __dirname,
+        ROOT,
         '@micro-app/plugin-deploy', // test
     ];
 }
