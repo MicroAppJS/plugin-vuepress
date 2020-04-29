ebfc3b42c300e12706b210a7f2f42cedfeff00ec
Zyao89 <zyao89@gmail.com>
2020-04-29 18:17:57 +0800

    art: remove bin

--- microapp/config.js
+++ microapp/config.js
@@ -6,11 +6,9 @@ const ROOT = path.resolve(__dirname, '../');
 const config = {};
 
 // 兼容两种启动方式
-if (!process.env.MICRO_APP_VUEPRESS_DIRECT_RUNNING) {
-    config.plugins = [
-        ROOT,
-        '@micro-app/plugin-deploy', // test
-    ];
-}
+config.plugins = [
+    ROOT,
+    '@micro-app/plugin-deploy', // test
+];
 
 module.exports = config;
