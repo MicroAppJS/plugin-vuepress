c8ef3f3ba7b596ee7f893011ea3675fb07d5a547
Zyao89 <zyao89@gmail.com>
2020-05-18 12:42:19 +0800

    fix: fixed watcher for blog

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,5 +8,11 @@ module.exports = {
         'micro-app.config.js',
         'micro-app.extra.config.js',
         `micro-app.${NAME}.config.js`,
+        'microapp/config.js',
+        'microapp/extra.config.js',
+        `microapp/${NAME}.config.js`,
+        'microapp/config/index.js',
+        'microapp/config/extra.js',
+        `microapp/config/${NAME}.js`,
     ],
 };
